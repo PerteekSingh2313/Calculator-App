a46136994db1ccaab3e84632e542ce3b66ef2453
Prateek Kumar Singh <prateeksingh5230@gmail.com>
2022-09-28 00:44:11 +0530

    Add ScreenShort and Modified some Feacture

--- app/src/main/java/com/example/prateeksingh/MainActivity.java
+++ app/src/main/java/com/example/prateeksingh/MainActivity.java
@@ -3,10 +3,12 @@ package com.example.prateeksingh;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     EditText etFistValue,etSecondValue;
@@ -30,42 +32,66 @@ public class MainActivity extends AppCompatActivity {
         add.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                int firstValue,secondValue,ans;
-                firstValue = Integer.parseInt(etFistValue.getText().toString());
-                secondValue = Integer.parseInt(etSecondValue.getText().toString());
-                ans = firstValue + secondValue;
-                tvAns.setText("Ans = "+ans);
+                if(!(TextUtils.isEmpty(etFistValue.getText().toString()) && TextUtils.isEmpty(etSecondValue.getText().toString()))){
+                    int firstValue,secondValue,ans;
+                    firstValue = Integer.parseInt(etFistValue.getText().toString());
+                    secondValue = Integer.parseInt(etSecondValue.getText().toString());
+                    ans = firstValue + secondValue;
+                    tvAns.setText("Addition = "+ans);
+                }
+                else {
+                    Toast.makeText(getApplicationContext(),"Please Enter Value",Toast.LENGTH_LONG).show();
+                }
             }
         });
         subtract.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 int firstValue,secondValue,ans;
-                firstValue = Integer.parseInt(etFistValue.getText().toString());
-                secondValue = Integer.parseInt(etSecondValue.getText().toString());
-                ans = firstValue - secondValue;
-                tvAns.setText("Ans = "+ans);
+
+                if(!(TextUtils.isEmpty(etFistValue.getText().toString()) && TextUtils.isEmpty(etSecondValue.getText().toString()))){
+                    firstValue = Integer.parseInt(etFistValue.getText().toString());
+                    secondValue = Integer.parseInt(etSecondValue.getText().toString());
+                    ans = firstValue - secondValue;
+                    tvAns.setText("Subtration = "+ans);
+                }
+                else {
+                    Toast.makeText(getApplicationContext(),"Please Enter Value",Toast.LENGTH_LONG).show();
+                }
+
             }
         });
         multiply.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                int firstValue,secondValue,ans;
-                firstValue = Integer.parseInt(etFistValue.getText().toString());
-                secondValue = Integer.parseInt(etSecondValue.getText().toString());
-                ans = firstValue * secondValue;
-                tvAns.setText("Ans = "+ans);
+                if(!(TextUtils.isEmpty(etFistValue.getText().toString()) && TextUtils.isEmpty(etSecondValue.getText().toString()))){
+                    int firstValue,secondValue,ans;
+                    firstValue = Integer.parseInt(etFistValue.getText().toString());
+                    secondValue = Integer.parseInt(etSecondValue.getText().toString());
+                    ans = firstValue * secondValue;
+                    tvAns.setText("Multiplication = "+ans);
+                }
+                else {
+                    Toast.makeText(getApplicationContext(),"Please Enter Value",Toast.LENGTH_LONG).show();
+                }
+
             }
         });
         divide.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                int firstValue,secondValue;
-                float ans;
-                firstValue = Integer.parseInt(etFistValue.getText().toString());
-                secondValue = Integer.parseInt(etSecondValue.getText().toString());
-                ans = (float)firstValue / secondValue;
-                tvAns.setText("Ans = "+ans);
+                if(!(TextUtils.isEmpty(etFistValue.getText().toString()) && TextUtils.isEmpty(etSecondValue.getText().toString()))){
+                    int firstValue,secondValue;
+                    double ans;
+                    firstValue = Integer.parseInt(etFistValue.getText().toString());
+                    secondValue = Integer.parseInt(etSecondValue.getText().toString());
+                    ans = (float)firstValue / secondValue;
+                    tvAns.setText("Division = "+ans);
+                }
+                else {
+                    Toast.makeText(getApplicationContext(),"Please Enter Value",Toast.LENGTH_LONG).show();
+                }
+
             }
         });
 
